f7ee2b39db827a045c8225d03a1ff28172468f81
Nikhil Reddy Avuthu <anr.2648@gmail.com>
2021-12-14 18:17:15 +0530

    add dark mode and top bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mat-tail-starter';
+  dark_mode: boolean = this.readLocalStorage();
+  toggleDarkMode() {
+    this.dark_mode = !this.dark_mode;
+    if (this.dark_mode) {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
+    }
+    this.setLocalStorage(this.dark_mode);
+  }
+  readLocalStorage(): boolean {
+    let key = localStorage.getItem("dark_mode");
+    if (key == null) {
+      return this.dark_mode;
+    } else {
+      return (key === 'true');
+    }
+  }
+  setLocalStorage(current_dark_mode_state: boolean) {
+    localStorage.setItem('dark_mode', current_dark_mode_state.toString());
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -9,6 +13,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppComponent
   ],
   imports: [
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule,
     BrowserModule,
     BrowserAnimationsModule
   ],
